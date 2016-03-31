b0e4cb2982d68ab15b58f57702a38d4f174ec920
Martin Horava <horavamartin@gmail.com>
2016-03-31 12:34:08 +0200

    Added init project

---
+++ src/main/java/net/horava/tracker/payment_tracker/App.java
@@ -0,0 +1,12 @@
+package net.horava.tracker.payment_tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+    }
+}
